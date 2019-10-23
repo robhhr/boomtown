a65bc92c7c24df5eda39a91dcc371209c134c6ca
Roberto H <robhhr@outlook.com>
2019-10-22 17:14:17 -0700

    prettier setup

--- server/config/application.js
+++ server/config/application.js
@@ -1,10 +1,10 @@
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const express = require('express');
-const fallback = require('express-history-api-fallback');
-const path = require('path');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const express = require("express");
+const fallback = require("express-history-api-fallback");
+const path = require("path");
 
-module.exports = (app) => {
+module.exports = app => {
   const PORT = process.env.PORT || 8080;
 
   /**
@@ -47,21 +47,21 @@ module.exports = (app) => {
 
   app.use(cookieParser());
 
-  if (process.env.NODE_ENV === 'production') {
-    const root = path.resolve(__dirname, '../public');
+  if (process.env.NODE_ENV === "production") {
+    const root = path.resolve(__dirname, "../public");
 
     // Serve the static front-end from /public when deployed
     app.use(express.static(root));
-    app.use(fallback('index.html', { root }));
+    app.use(fallback("index.html", { root }));
   }
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     // Allow requests from dev server address
     const corsConfig = {
-      origin: 'http://localhost:3000',
+      origin: "http://localhost:3000",
       credentials: true
     };
-    app.set('CORS_CONFIG', corsConfig);
+    app.set("CORS_CONFIG", corsConfig);
 
     // Allow requests from dev server address
     app.use(cors(corsConfig));
--- server/main.js
+++ server/main.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const chalk = require('chalk');
+const express = require("express");
+const chalk = require("chalk");
 
-const config = require('./config/application');
+const config = require("./config/application");
 
 const app = express();
 const PORT = config(app);
 
-const initDB = require('./config/postgres');
+const initDB = require("./config/postgres");
 const postgres = initDB(app);
 
-let pgResource = require('./api/pg-resource');
+let pgResource = require("./api/pg-resource");
 pgResource = pgResource(postgres);
 
 /**
@@ -24,13 +24,13 @@ pgResource = pgResource(postgres);
  *  initApollo({ app, pgResource })
  */
 
-postgres.on('error', (err, client) => {
-  console.error('Unexpected error on idle postgres client', err);
+postgres.on("error", (err, client) => {
+  console.error("Unexpected error on idle postgres client", err);
   process.exit(-1);
 });
 
 const server = app.listen(PORT, () => {
-  console.log(`>> ${chalk.blue('Express running:')} http://localhost:${PORT}`);
+  console.log(`>> ${chalk.blue("Express running:")} http://localhost:${PORT}`);
 
   /**
    *  @TODO: Initialize Apollo Server
@@ -43,6 +43,6 @@ const server = app.listen(PORT, () => {
    */
 });
 
-server.on('error', err => {
+server.on("error", err => {
   console.log(err);
 });
