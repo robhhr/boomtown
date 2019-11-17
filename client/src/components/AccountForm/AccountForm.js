7bfea40795fd50667afe795ae32487f619d3e800
Roberto H <robhhr@outlook.com>
2019-11-16 23:12:19 -0800

    wrap forms in react final form

--- client/src/components/AccountForm/AccountForm.js
+++ client/src/components/AccountForm/AccountForm.js
@@ -1,6 +1,7 @@
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
+import { Form, Field } from "react-final-form";
 import Grid from "@material-ui/core/Grid";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
@@ -9,7 +10,6 @@ import Typography from "@material-ui/core/Typography";
 /**
  * @TODO: Uncomment the following lines when authentication is added to the form
  *
- * import { Form, Field } from 'react-final-form'
  *
  * import {
  *    LOGIN_MUTATION,
@@ -34,96 +34,109 @@ class AccountForm extends Component {
     const { classes } = this.props;
 
     return (
-      // @TODO: Wrap in Final Form <Form />
-      <form
-        onSubmit={() => {
-          console.log("Submitted");
+      <Form
+        onSubmit={values => {
+          console.log(values);
         }}
-        className={classes.accountForm}
-      >
-        {!this.state.formToggle && (
-          <FormControl fullWidth className={classes.formControl}>
-            <InputLabel htmlFor="fullname">Username</InputLabel>
-            {/* @TODO: Wrap in a Final Form <Field /> */}
-            <Input
-              id="fullname"
-              type="text"
-              inputProps={{
-                autoComplete: "off"
-              }}
-              // value={''}
-            />
-            {/* @TODO: Close Final Form <Field /> */}
-          </FormControl>
-        )}
-        <FormControl fullWidth className={classes.formControl}>
-          <InputLabel htmlFor="email">Email</InputLabel>
-          {/* @TODO: Wrap in a Final Form <Field /> */}
-          <Input
-            id="email"
-            type="text"
-            inputProps={{
-              autoComplete: "off"
-            }}
-            // value={''}
-          />
-          {/* @TODO: Close Final Form <Field /> */}
-        </FormControl>
-        <FormControl fullWidth className={classes.formControl}>
-          <InputLabel htmlFor="password">Password</InputLabel>
-          {/* @TODO: Wrap in a Final Form <Field /> */}
-          <Input
-            id="password"
-            type="password"
-            inputProps={{
-              autoComplete: "off"
-            }}
-            // value={''}
-          />
-          {/* @TODO: Close Final Form <Field /> */}
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <Grid
-            container
-            direction="row"
-            justify="space-between"
-            alignItems="center"
-          >
-            <Button
-              type="submit"
-              className={classes.formButton}
-              variant="contained"
-              size="large"
-              color="secondary"
-              disabled={
-                false // @TODO: This prop should depend on pristine or valid state of form
-              }
-            >
-              {this.state.formToggle ? "Enter" : "Create Account"}
-            </Button>
-            <Typography>
-              <button
-                className={classes.formToggle}
-                type="button"
-                onClick={() => {
-                  // @TODO: Reset the form on submit
-                  this.setState({
-                    formToggle: !this.state.formToggle
-                  });
-                }}
+        // validate={}
+        render={({ handleSubmit, form }) => (
+          <form onSubmit={handleSubmit} className={classes.accountForm}>
+            {!this.state.formToggle && (
+              <FormControl fullWidth className={classes.formControl}>
+                <InputLabel htmlFor="fullname">Username</InputLabel>
+                <Field name="fullname">
+                  {/* @TODO: Wrap in a Final Form <Field /> */}
+                  {({ input }) => (
+                    <Input
+                      id="fullname"
+                      type="text"
+                      inputProps={{
+                        autoComplete: "off"
+                      }}
+                      value={Input.value}
+                    />
+                  )}
+                </Field>
+                {/* @TODO: Close Final Form <Field /> */}
+              </FormControl>
+            )}
+            <FormControl fullWidth className={classes.formControl}>
+              <InputLabel htmlFor="email">Email</InputLabel>
+              <Field name="email">
+                {/* @TODO: Wrap in a Final Form <Field /> */}
+                {({ input }) => (
+                  <Input
+                    id="email"
+                    type="text"
+                    inputProps={{
+                      autoComplete: "off"
+                    }}
+                    value={Input.value}
+                  />
+                )}
+                {/* @TODO: Close Final Form <Field /> */}
+              </Field>
+            </FormControl>
+            <FormControl fullWidth className={classes.formControl}>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Field name="password">
+                {/* @TODO: Wrap in a Final Form <Field /> */}
+                {({ input }) => (
+                  <Input
+                    id="password"
+                    type="password"
+                    inputProps={{
+                      autoComplete: "off"
+                    }}
+                    value={Input.value}
+                  />
+                )}
+                {/* @TODO: Close Final Form <Field /> */}
+              </Field>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <Grid
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="center"
               >
-                {this.state.formToggle
-                  ? "Create an account."
-                  : "Login to existing account."}
-              </button>
+                <Button
+                  type="submit"
+                  className={classes.formButton}
+                  variant="contained"
+                  size="large"
+                  color="secondary"
+                  disabled={
+                    false // @TODO: This prop should depend on pristine or valid state of form
+                  }
+                >
+                  {this.state.formToggle ? "Enter" : "Create Account"}
+                </Button>
+                <Typography>
+                  <button
+                    className={classes.formToggle}
+                    type="button"
+                    onClick={() => {
+                      // @TODO: Reset the form on submit
+                      this.setState({
+                        formToggle: !this.state.formToggle
+                      });
+                    }}
+                  >
+                    {this.state.formToggle
+                      ? "Create an account."
+                      : "Login to existing account."}
+                  </button>
+                </Typography>
+              </Grid>
+            </FormControl>
+            <Typography className={classes.errorMessage}>
+              {/* @TODO: Display sign-up and login errors */}
             </Typography>
-          </Grid>
-        </FormControl>
-        <Typography className={classes.errorMessage}>
-          {/* @TODO: Display sign-up and login errors */}
-        </Typography>
-      </form>
-      // @TODO: Close Final Form <Form />
+          </form>
+        )}
+      />
     );
   }
 }
