95c073f552b77ec485a2c5f1f87737dba84c105c
Roberto H <robhhr@outlook.com>
2019-11-22 13:43:18 -0800

    add routes redirect

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -16,21 +16,13 @@ import {
 const Routes = () => {
   return (
     <Router>
-      {/* @TODO: Add your menu component here */}
       <Switch>
         <Route exact path="/items" component={Items} />
         <Route path="/welcome" component={Home} />
         <Route path="/share" component={Share} />
         <Route path="/profile" component={Profile} />
         <Route path="/profile/user:Id" component={Profile} />
-        {/**
-         * @TODO: Define routes here for: /items, /profile, /profile/:userid, and /share
-         *
-         * Provide a wildcard redirect to /items for any undefined route using <Redirect />.
-         *
-         * Later, we'll add logic to send users to one set of routes if they're logged in,
-         * or only view the /welcome page if they are not.
-         */}
+        <Redirect from="/" to="/welcome" /* to='/items' */ />
       </Switch>
     </Router>
   );
